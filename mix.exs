0becd57f11662301fe8958b855c1d2f314ff7704
Michael Niepel <michael.niepel@gmail.com>
2016-07-14 21:53:43 +0200

    removes migrate (no migrations on mongo) from test

--- mix.exs
+++ mix.exs
@@ -50,6 +50,6 @@ defmodule Rumbl.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create --quiet", "test"]]
   end
 end
