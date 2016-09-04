f56c18526a6c8895d5580da64f5572c2f702ede0
Michael Niepel <michael.niepel@gmail.com>
2016-09-04 11:41:21 +0200

    simple GenServer

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -1,29 +1,32 @@
 defmodule Rumbl.Counter do
+  # use generic server
+  use GenServer
+
   # API
-  def inc(pid), do: send(pid, :inc)
-  def dec(pid), do: send(pid, :dec)
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
-    receive do
-      {^ref, val} -> val
-    after timeout -> exit(:timeout)
-    end
+  def inc(pid), do: GenServer.cast(pid, :inc)
+  def dec(pid), do: GenServer.cast(pid, :dec)
+  def val(pid), do: GenServer.call(pid, :val)
+
+  def start_link(initial_val) do
+    GenServer.start_link(__MODULE__, initial_val)
   end
+
 
   # SERVER
-  def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
+  def init(initial_val) do
+    {:ok, initial_val}
   end
 
-  # tail recursive function
-  defp listen(val) do
-    receive do
-      :inc -> listen(val + 1)
-      :dec -> listen(val - 1)
-      {:val, sender, ref} ->
-        send sender, {ref, val}
-        listen(val)
-    end
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
   end
+
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
+  end
+
 end
