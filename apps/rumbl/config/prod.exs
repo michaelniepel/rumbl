bbdb382d0966624db81131f695d46285781aa066
Michael Niepel <michael.niepel@gmail.com>
2016-10-24 15:32:51 +0200

    use string for pool size param

--- apps/rumbl/config/prod.exs
+++ apps/rumbl/config/prod.exs
@@ -23,7 +23,7 @@ config :rumbl, Rumbl.Endpoint,
 config :rumbl, Rumbl.Repo,
   adapter: Mongo.Ecto,
   url: System.get_env("MONGODB_URI"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || 20),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
 
 # Do not print debug messages in production
 config :logger, level: :info
